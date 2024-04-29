8f07c9e229c1ffa08bc1053cd2853a3d8d07ef4e
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-04-29 15:52:14 +0530

    Add files via upload

---
+++ Flyod_warshall.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define V 4
+#define INF 99999
+
+void printSolution(int dist[][V]);
+
+void floydWarshall(int dist[][V]) {
+    int i, j, k;
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V]) {
+    printf("The following matrix shows the shortest distances between every pair of vertices\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int graph[V][V];
+    printf("Enter the weighted adjacency matrix for the graph (%d x %d):\n", V, V);
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+            if (i != j && graph[i][j] == 0) 
+                graph[i][j] = INF;
+        }
+    }
+    floydWarshall(graph);
+    return 0;
+}
