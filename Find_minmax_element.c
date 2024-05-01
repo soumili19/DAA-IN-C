7060c2fd047b52b2f89c22dcd8508d576c729d70
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-01 19:55:42 +0530

    Add files via upload

---
+++ Find_minmax_element.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+void minmax(int arr[],int n){
+    int i;
+    int  min =INT_MAX, max=INT_MIN,;
+    for(i=0;i<n;i++){
+        if(arr[i]<min)
+        min=arr[i];
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    printf("The minimum number of the elements %d",min);
+    printf("\n");
+    printf("The maximum number of the elements %d",max);
+    printf("\n");
+}
+int main(){
+    int i,n;
+    printf("Enter the number of the elemnts : \n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements : \n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    minmax(arr,n);
+    return 0;
+}
