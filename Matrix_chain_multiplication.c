a0ec6e98ace4aa8fcfb6632da5fa4d6e18661fb1
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-15 12:34:02 +0530

    Update Matrix_chain_multiplication.c

--- Matrix_chain_multiplication.c
+++ Matrix_chain_multiplication.c
@@ -1,35 +1,46 @@
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
 int matrixchain(int p[], int n) {
+    int i, j, k, d, q, min;
     int m[n][n];
-    int i, j, k, L, q;
-    for (i = 0; i < n; i++) {
+    int s[n][n];
+    
+    for(i = 0; i < n; i++) {
         m[i][i] = 0;
+        s[i][i] = 0;
     }
-    for (L = 2; L < n; L++) {
-        for (i = 1; i < n - L + 1; i++) {
-            j = i + L - 1;
-            m[i][j] = INT_MAX;
-            for (k = i; k < j; k++) {
+
+    for(d = 1; d < n - 1; d++) {
+        for(i = 1; i < n - d; i++) {
+            j = i + d;
+            min = INT_MAX;
+            for(k = i; k <j; k++) {
                 q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (q < m[i][j])
-                    m[i][j] = q;
+                if(q < min) {
+                    min = q;
+                    s[i][j] = k;
+                }
             }
+            m[i][j] = min;
         }
     }
+    
     return m[1][n - 1];
 }
+
 int main() {
-    int n, i;
+    int i, n;
     printf("Enter the number of matrices: ");
     scanf("%d", &n);
-    int *p = (int *)malloc((n + 1) * sizeof(int));
-    printf("Enter the dimensions of %d matrices: ", n);
-    for (i = 0; i <= n; i++) {
+    int *p = (int*)malloc((n + 1) * sizeof(int));
+    printf("Enter the dimensions of the matrices: ");
+    for(i = 0; i <= n; i++) {
         scanf("%d", &p[i]);
     }
-    printf("Minimum number of multiplications is %d\n", matrixchain(p, n + 1));
+    printf("The minimum number of scalar multiplications is %d\n", matrixchain(p, n + 1));
     free(p);
     return 0;
 }
+ 
