e6b1acd09d839ed1ea815fd365763dff4f8b2194
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-25 19:43:50 +0530

    Update Find_minmaxelement.c

--- Find_minmaxelement.c
+++ Find_minmaxelement.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<limits.h>
-//min max search//
-void minmax(int arr[],int n){
-    int i;
-    int  min =INT_MAX, max=INT_MIN,;
-    for(i=0;i<n;i++){
-        if(arr[i]<min)
-        min=arr[i];
-        if(arr[i]>max)
-        max=arr[i];
-    }
-    printf("The minimum number of the elements %d",min);
-    printf("\n");
-    printf("The maximum number of the elements %d",max);
-    printf("\n");
+void Findminmax(int arr[],int n){
+	int i;
+	int min=INT_MAX;
+	int max=INT_MIN;
+	for(i=0;i<=n;i++){
+		if(arr[i]>max)
+		{
+			max=arr[i];
+		}
+		if(arr[i]<min){
+			min=arr[i];
+		}
+	}
+	printf("The maximum element is %d",max);
+	printf("The minimum element is %d",min);
 }
-int main(){
-    int i,n;
-    printf("Enter the number of the elemnts : \n");
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter the elements : \n");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    minmax(arr,n);
-    return 0;
+int main()
+{
+	int n,i;
+	printf("Enter the number of the elements : ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the number : ");
+	for(i=0;i<=n;i++){
+		scanf("%d",&arr[i]);
+	}
+	Findminmax(arr,n);
+	return 0;
 }
